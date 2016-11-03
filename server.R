3386575301bac03b4fbca06f8bf15ade8fa27ecf
apanliao <andrew.43@gmail.com>
2016-11-03 22:50:29 +0800

    加入CDF

--- server.R
+++ server.R
@@ -8,41 +8,41 @@ library(shiny)
 require(MCMCpack)
 
 function(input, output) {
-  # selected.V <- reactive({input$V})
-  # selected.nu <- reactive({input$nu})
-  # selected.xmax <- reactive({input$xmax})
-  # alpha <- selected.nu() / 2
-  # beta <- selected.V() * selected.nu() / 2
-  # xseq <- seq(0, selected.xmax(), length.out = 2000)
   output$distPlot <- renderPlot({
     alpha <- as.numeric(input$nu) / 2
     beta <- as.numeric(input$V) * as.numeric(input$nu) / 2
-    xseq <- seq(0, as.numeric(input$xmax), length.out = 2000)
-    DV <- MCMCpack::dinvgamma(xseq, shape = alpha, scale = beta)
-    plot(DV ~ xseq,
-         type = "l",
-         xlab = "x",
-         ylab = "Probability density")
+    xmax <- 10 ^ as.numeric(input$xmax)
+    xseq <- seq(0.00000001, xmax, length.out = 5000)
+    DV.pdf <- MCMCpack::dinvgamma(xseq, shape = alpha, scale = beta)
+    DV.cdf <- cumsum(DV.pdf) * (xseq[2] - xseq[1])
+    par(mfrow = c(2, 1), las = 1)
+    plot(
+      DV.pdf ~ xseq,
+      type = "l",
+      xlab = "x",
+      ylab = "",
+      ylim = c(0, max(DV.pdf)),
+      main = paste0(
+        "PDF of inverse-gamma distribution (α = ",
+        alpha,
+        ", β = ",
+        beta,
+        ")"
+      )
+    )
+    plot(
+      DV.cdf ~ xseq,
+      type = "l",
+      xlab = "x",
+      ylab = "",
+      ylim = c(0, max(DV.cdf)),
+      main = paste0(
+        "CDF of inverse-gamma distribution (α = ",
+        alpha,
+        ", β = ",
+        beta,
+        ")"
+      )
+    )
   })
 }
-
-
-# https://en.wikipedia.org/wiki/Inverse-Wishart_distribution
-# A univariate specialization of the inverse-Wishart distribution is the
-# inverse-gamma distribution. With p=1 (i.e. univariate) and \alpha = \nu/2,
-# \beta = \mathbf{\Psi}/2 and x=\mathbf{X} the probability density function
-# of the inverse-Wishart distribution becomes
-
-# https://en.wikipedia.org/wiki/Inverse-gamma_distribution
-# Inv-Gamma(alpha, beta)
-
-# alpha = nu / 2
-# beta = V * nu / 2
-
-# nu = alpha * 2
-# V = beta / alpha
-
-# An inverse gamma random variable with shape a and scale b has
-# mean b/(a-1) (assuming a>1) and
-# variance (b^2)/((a-1)^2 (a-2)) (assuming a>2).
-# The parameterization is consistent with the Gamma Distribution in the stats package.
--- ui.R
+++ ui.R
@@ -13,47 +13,54 @@ library(shiny)
 
 
 
-fluidPage(verticalLayout(
+fluidPage(
   titlePanel(
-    "Inverse Gamma Distribution (visualization of prior of G-/R- structure for MCMCglmm users)"
+    "Inverse Gamma Distribution"
   ),
-  wellPanel(
-    helpText(
-      HTML(
-        "<p>See <a href = 'https://en.wikipedia.org/wiki/Inverse-gamma_distribution'>Inverse Gamma Distribution @ Wikipedia</a> to know the details.</p><p>Auther: Chen-Pan Liao <code>andrew.43@gmail.com</code></p>"
+  sidebarLayout(
+    sidebarPanel(
+      helpText(
+        HTML(
+          "<p>This Shiny app provide the visualized PDF/CDF of inverse gamma distribution, which is used for prior of G-/R- structure for MCMCglmm users. See <a href = 'https://en.wikipedia.org/wiki/Inverse-gamma_distribution'>Inverse Gamma Distribution @ Wikipedia</a> to know the details.</p><p>Auther: Chen-Pan Liao <code>andrew.43@gmail.com</code></p>"
+        )
+      ),
+      selectInput(
+        'V',
+        'Expected value of variance (V) = β / α',
+        as.character(c(0.01, 0.25, 1, 4, 16, 25, 100, 225, 400, 900, 2500, 10000)),
+        selected = 1
+      ),
+      selectInput(
+        'nu',
+        'Degree of belief parameter (nu) = α * 2',
+        as.character(c(
+          # sprintf("%1.6f", 0.000002),
+          sprintf("%1.5f", 0.00002),
+          sprintf("%1.4f", 0.0002),
+          0.002,
+          0.02,
+          0.2,
+          1,
+          2,
+          5,
+          10,
+          20,
+          100
+        )),
+        selected = 0.2
+      ),
+      sliderInput(
+        "xmax",
+        "Maximal x-axis value",
+        min = -5,
+        max = 7,
+        value = 1,
+        step = 1,
+        pre = "10 ^ (",
+        post = ")",
+        sep = ""
       )
     ),
-    selectInput(
-      'V',
-      'Expected value of variance (V) = beta / alpha',
-      as.character(c(0.1, 1, 2, 5, 10, 20, 50, 100)),
-      selected = 1
-    ),
-    selectInput(
-      'nu',
-      'Degree of belief parameter (nu) = alpha * 2',
-      as.character(c(
-        sprintf("%1.4f", 0.0002),
-        0.002,
-        0.002,
-        0.02,
-        0.2,
-        1,
-        2,
-        5,
-        10,
-        20,
-        100
-      )),
-      selected = 0.2
-    ),
-    sliderInput(
-      "xmax",
-      "Maximal x-axis value",
-      min = 0.1,
-      max = 1000,
-      value = 10
-    )
-  ),
-  plotOutput('distPlot')
-))
+    mainPanel(plotOutput('distPlot', height = "700px"))
+  )
+)
